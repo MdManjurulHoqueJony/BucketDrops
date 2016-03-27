30148fb83f97e0689f440551e67a641cac9beca3
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-03-27 11:13:26 +0600

    Adding Toolbar & Glide

--- app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/bucketdrops/MainActivity.java
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/bucketdrops/MainActivity.java
@@ -2,12 +2,28 @@ package com.wordpress.jonyonandroidcraftsmanship.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar = null;
+    private ImageView ivBackground = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    private void initialize() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ivBackground = (ImageView) findViewById(R.id.ivBackground);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(ivBackground);
     }
 }
