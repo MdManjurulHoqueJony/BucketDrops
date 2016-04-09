4721116f08855feaeb891019c27fe58d8bb12757
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-04-09 09:31:42 +0600

    Disable Swipe For The Footer

--- app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/bucketdrops/adapters/SimpleTouchCallback.java
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/bucketdrops/adapters/SimpleTouchCallback.java
@@ -1,5 +1,6 @@
 package com.wordpress.jonyonandroidcraftsmanship.bucketdrops.adapters;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -32,7 +33,23 @@ public class SimpleTouchCallback extends ItemTouchHelper.Callback {
     }
 
     @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (viewHolder instanceof DropsAdapter.DropHolder) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (viewHolder instanceof DropsAdapter.DropHolder) {
+            super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-        mSwipeListener.onSwipe(viewHolder.getAdapterPosition());
+        if (viewHolder instanceof DropsAdapter.DropHolder) {
+            mSwipeListener.onSwipe(viewHolder.getAdapterPosition());
+        }
     }
 }
